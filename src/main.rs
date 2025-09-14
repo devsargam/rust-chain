8f47ba9bc71130c37b25762d99edbbdf51d02a21
Sargam Poudel <sargampoudel100@gmail.com>
2025-09-14 07:48:16 +0530

    working v2

--- src/main.rs
+++ src/main.rs
@@ -7,59 +7,66 @@ use std::sync::{
 };
 use std::thread;
 
-#[derive(Clone)]
-struct BlockHeader {
+#[derive(Debug, Clone)]
+struct Block {
+    index: u64,
     prev_hash: [u8; 32],
     merkle_root: [u8; 32],
     nonce: u64,
-}
-
-fn hash_header(header: &BlockHeader) -> [u8; 32] {
-    let mut hasher = Sha256::new();
-    hasher.update(&header.prev_hash);
-    hasher.update(&header.merkle_root);
-    hasher.update(&header.nonce.to_le_bytes());
-    let out = hasher.finalize();
-    let mut arr = [0u8; 32];
-    arr.copy_from_slice(&out);
-    arr
+    hash: [u8; 32],
 }
 
 fn meets_target(hash: &[u8; 32], difficulty_zero_bytes: usize) -> bool {
     hash.iter().take(difficulty_zero_bytes).all(|&b| b == 0)
 }
 
-fn main() {
-    let workers = 10;
-    let difficulty = 3;
+fn calculate_hash(
+    index: u64,
+    prev_hash: &[u8; 32],
+    merkle_root: &[u8; 32],
+    nonce: u64,
+) -> [u8; 32] {
+    let mut hasher = Sha256::new();
+    hasher.update(&index.to_le_bytes());
+    hasher.update(prev_hash);
+    hasher.update(merkle_root);
+    hasher.update(&nonce.to_le_bytes());
+    let out = hasher.finalize();
+    let mut arr = [0u8; 32];
+    arr.copy_from_slice(&out);
+    arr
+}
 
-    let mut rng = rand::rng();
-
-    let header = BlockHeader {
-        prev_hash: rng.random(),
-        merkle_root: rng.random(),
-        nonce: 0,
-    };
-
+fn mine_blocks(index: u64, prev_hash: [u8; 32], difficulty: usize, workers: usize) -> Block {
     let found = Arc::new(AtomicBool::new(false));
     let result = Arc::new(Mutex::new(None));
 
     let (tx, rx) = mpsc::channel();
 
+    let merkle_root: [u8; 32] = rand::random();
+
     for i in 0..workers {
-        let mut local_header = header.clone();
         let found = found.clone();
         let result = result.clone();
         let tx = tx.clone();
+        let prev_hash = prev_hash.clone();
+        let merkle_root = merkle_root.clone();
+
         thread::spawn(move || {
             let mut nonce = i as u64;
             while !found.load(Ordering::Relaxed) {
-                local_header.nonce = nonce;
-                let h = hash_header(&local_header);
-                if meets_target(&h, difficulty) {
+                let hash = calculate_hash(index, &prev_hash, &merkle_root, nonce);
+                if meets_target(&hash, difficulty) {
                     found.store(true, Ordering::Relaxed);
-                    *result.lock().unwrap() = Some((nonce, h));
-                    tx.send((i, nonce)).unwrap();
+                    let block = Block {
+                        index,
+                        prev_hash,
+                        merkle_root,
+                        nonce,
+                        hash,
+                    };
+                    *result.lock().unwrap() = Some(block.clone());
+                    tx.send(block).unwrap();
                     break;
                 }
                 nonce += workers as u64;
@@ -67,8 +74,38 @@ fn main() {
         });
     }
 
-    let (worker_id, nonce) = rx.recv().unwrap();
-    println!("Worker {worker_id} found valid nonce: {nonce}");
-    let res = result.lock().unwrap();
-    println!("Hash: {:x?}", res.as_ref().unwrap().1);
+    rx.recv().unwrap()
+}
+
+fn main() {
+    let workers = 10;
+    let difficulty = 3;
+    let chain_max_length = 10;
+    let mut chain: Vec<Block> = Vec::new();
+
+    let mut rng = rand::rng();
+
+    // Create the initial genesis
+    let genesis = Block {
+        prev_hash: rng.random(),
+        merkle_root: rng.random(),
+        nonce: 0,
+        hash: rng.random(),
+        index: 0,
+    };
+    chain.push(genesis);
+
+    println!("Starting mining with {} workers...", workers);
+    for height in 1..chain_max_length {
+        let prev_hash = chain.last().unwrap().hash;
+        println!("\n⛏️  Mining block {}", height);
+
+        let block = mine_blocks(height, prev_hash, difficulty, workers);
+        println!(
+            "✅ Block {} mined! Nonce: {}, Hash: {:x?}",
+            block.index, block.nonce, block.hash
+        );
+
+        chain.push(block);
+    }
 }
